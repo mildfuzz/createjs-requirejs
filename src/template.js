6f3e0e968b5b5dc1fcc2f04b432892b7b947cad9
John Farrow <john@mildfuzz.com>
2013-12-17 11:24:02 +0000

    added readme, refactored a little

--- src/template.js
+++
@@ -1,9 +0,0 @@
-/*
- * Author: john.farrow@aerian.com
- */
-define(['libs/movieclip'],function(createjs){
-	var spritesheet; //container for spritesheet
-  
-	{{ spritesheet }}
-	return spritesheet;
-});
