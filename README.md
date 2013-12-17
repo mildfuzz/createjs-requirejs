f8e9af7c332af7ffb72f04a3d7453c0999873fad
John Farrow <john@mildfuzz.com>
2013-12-17 11:22:47 +0000

    added readme, refactored a little

---
+++ README.md
@@ -0,0 +1,12 @@
+createjs-require
+================
+A quick and dirty way to wrap the output of [CreateJs Toolkit for Flash](https://www.adobe.com/products/flash/flash-to-html5.html) in a [RequireJS](http://www.requirejs.org/) module define block.
+
+About
+============
+The point of Simple is to be simple. The blog is 1 file (excluding resources) with a few simple pure-python dependancies (Flask, Sqlalchemy, Markdown), it doesn't require a database server, has a small footprint and is very fast (Can easily handle 300+ requests per second).
+
+Usage
+============
+Publish .fla files with toolkit for flash, then run `createjs-require` from within the folder the .fla published to.
+
--- index.js
+++ index.js
@@ -1,20 +1,44 @@
 #!/usr/bin/env node
 
-var fs = require('fs');
-var mu = require('mu2');
-
-mu.root = __dirname + '/src'
+var fs = require("fs");
+var _ = require("underscore-node");
 
 
 
-//load toolkitjs export && locate and insert spritesheet marker
-var file = fs.readFileSync( "test/sample.js", 'utf8' ).replace(' = ',' = spritesheet = ');
+//set dir
+var dir = process.argv[2] || "./";
+
+var newFileDir = dir+"converted/";
+
+//create 'converted' directory
+try{ fs.mkdirSync(newFileDir); } catch(e){}
+
+//get js files
+var jsFiles = _.reject(fs.readdirSync(dir),function(file){
+	return !file.match(".js");
+});
+
+var moduleWrap = function(str){
+	var file = "/*\n* Author: john.farrow@aerian.com\n*/\ndefine(['libs/movieclip'],function(createjs){\nvar spritesheet; //container for spritesheet";
+		file += str.replace(" = "," = spritesheet = ");
+		file += "\nreturn spritesheet;\n});";
+	return file;
+};
+
+var processFile = function(name){
+	var filePath = dir+name,
+		file = fs.readFileSync( filePath, "utf8" ),
+		wrappedFile = moduleWrap(file),
+		newFilePath = newFileDir+name.replace(/\s/g,"-");
+
+	fs.writeFileSync( newFilePath, wrappedFile);
+};
+
+//process all JS files
+_(jsFiles).each(processFile);
 
 
 
-//compile spritesheet
-var data = {spritesheet: file};
-mu.compileAndRender('template.js', data)
-	.on('data', function (data) {
-	console.log(data.toString());
-	});
+
+
+
